c967afe8f1d05ad153032e2fbcd250a4f96551bf
Lesan <lesan2d@qq.com>
2024-09-03 15:21:13 +0800

    chore: :truck: workspace package 重命名

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import SetsUi from 'sets-ui';
+import SetsUi from '@sets-ui/main';
 import GlobalComponents from './components';
 import Demos from '../../demos';
 
