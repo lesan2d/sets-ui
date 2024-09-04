95b83702e0035ab63494b51522512dea191418e1
Lesan <lesan2d@qq.com>
2024-09-04 11:33:14 +0800

    build: :rocket: 0.0.19 publish，添加 atomic/animation 单独导入

--- scripts/build-theme/webpack.config.cjs
+++ scripts/build-theme/webpack.config.cjs
@@ -9,6 +9,8 @@ module.exports = () => {
 		entry: () => {
 			const entry = {
 				index: path.resolve(__dirname, '../../packages/theme-default'),
+				animation: path.resolve(__dirname, '../../packages/theme-default/animation.scss'),
+				atomic: path.resolve(__dirname, '../../packages/theme-default/atomic.scss'),
 			};
 			const componentsDir = path.resolve(__dirname, '../../packages/theme-default/src');
 			const componentsName = fs.readdirSync(componentsDir);
