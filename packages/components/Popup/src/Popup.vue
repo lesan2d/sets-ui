22f7b5a81c3381d9f4f5df6f8ca592c35ccfb557
-g <-g>
2025-02-10 01:49:32 +0800

    fix(component): :bug: Popup 组件修复动画内 transform 属性对 Containing Block 的影响

--- packages/components/Popup/src/Popup.vue
+++ packages/components/Popup/src/Popup.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import type { DirectionType } from '#/component';
-import { ref, computed, watch, nextTick } from 'vue';
+import { ref, computed, watch, } from 'vue';
 import { genBEMClass } from '@packages/utils';
-import { useAnimationReset, useAnimationReverse } from '@packages/composables';
+import { useAnimationReverse } from '@packages/composables';
 import { useTheme } from '@sets-ui/config';
 import { SOverlay } from '@sets-ui/components/Overlay';
 import { SButton } from '@sets-ui/components/Button';
@@ -37,8 +37,9 @@ const emit = defineEmits(['update:modelValue', 'close', 'closed']);
 const extendsClass = genBEMClass('s-popup', [...themeName].filter((p) => Boolean(p)) as Array<string>);
 
 const shouldVisible = ref(props.modelValue); // 不确定的弹窗状态(判断动画过渡后才能确定显示状态)
+const opened = ref(false); // 弹窗打开中
 const closed = ref(false); // 弹窗关闭中
-let animationPlaying = false; // 过渡动画播放中
+let animationPlaying = ref(false); // 过渡动画播放中
 let timer: NodeJS.Timeout | number = 0;
 
 const { style: animationReverseStyle } = useAnimationReverse(closed);
@@ -81,33 +82,43 @@ const clearTimer = () => {
 const changeShouldVisible = (val: boolean) => {
   if (timer) clearTimer();
 
+  opened.value = val;
   closed.value = !val;
   if (!val) emit('close'); // 弹窗关闭回调
 
   timer = setInterval(() => {
-    if (animationPlaying) return; // 动画播放中时继续轮询
+    if (animationPlaying.value) return; // 动画播放中时继续轮询
+    opened.value = false; // 重置开启中状态
     closed.value = false; // 重置关闭中状态
     shouldVisible.value = val; // 更新弹窗显示
     if (timer) clearTimer();
     if (!val) emit('closed'); // 关闭动画结束回调
   }, 100);
 };
+
+const handleClickOverlay = () => {
+  if (props.closeOnClickOverlay) overlayVisible.value = false;
+}
+
 const handleClose = () => {
   visible.value = false;
 };
+
 const handleAnimationstart = () => {
-  animationPlaying = true;
+  animationPlaying.value = true;
 };
+
 const handleAnimationend = () => {
-  animationPlaying = false;
+  animationPlaying.value = false;
 }
 </script>
 
 <template>
-  <s-overlay v-if="props.overlay" v-model="overlayVisible" :destroy-on-close="props.destroyOnClose"
-    :close-on-click-overlay="props.closeOnClickOverlay" />
-  <div v-if="props.destroyOnClose ? shouldVisible : true" v-show="shouldVisible" class="s-popup"
-    :class="[extendsClass, props.direction]" v-bind="$attrs">
+  <s-overlay v-if="props.overlay" v-model="overlayVisible" :destroy-on-close="props.destroyOnClose" />
+  <div v-if="props.destroyOnClose ? shouldVisible : true" v-show="shouldVisible" class="s-popup" :class="[extendsClass, props.direction, {
+    'opened': opened,
+    'closed': closed,
+  }]" v-bind="$attrs" @click="handleClickOverlay">
     <div class="s-popup--wrapper" :style="animationStyle" @animationstart.self="handleAnimationstart"
       @animationend.self="handleAnimationend">
       <SButton v-if="props.showClose" text circle class="btn-close" @click="handleClose">
@@ -124,15 +135,14 @@ const handleAnimationend = () => {
   --s-popup-z-index: 1;
   position: fixed;
   z-index: calc(var(--z-index-top) + var(--s-popup-z-index));
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 
   &--wrapper {
-    display: grid;
+    display: inline-grid;
     grid-template-rows: auto 1fr auto;
-    width: 100%;
-    height: 100%;
     padding: var(--s-popup-padding);
     background-color: var(--base-color-lighter);
 
@@ -144,34 +154,41 @@ const handleAnimationend = () => {
     }
   }
 
+  // 默认居中
+  &:not(.ttb, .rtl, .btt, .ltr) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
   &.ttb {
-    width: 100%;
-    top: 0;
-    transform: translate(-50%, 0%);
+    .s-popup--wrapper {
+      width: 100%;
+    }
   }
 
   &.rtl {
-    height: 100%;
-    top: 0;
-    left: auto;
-    right: 0;
-    transform: translate(0%, 0%);
+    display: flex;
+    justify-content: flex-end;
+
+    .s-popup--wrapper {
+      height: 100%;
+    }
   }
 
   &.btt {
-    width: 100%;
-    top: auto;
-    bottom: 0;
-    transform: translate(-50%, 0%);
+    display: flex;
+    align-items: flex-end;
+
+    .s-popup--wrapper {
+      width: 100%;
+    }
   }
 
   &.ltr {
-    height: 100%;
-    top: 0;
-    left: 0;
-    right: auto;
-    transform: translate(0%, 0%);
+    .s-popup--wrapper {
+      height: 100%;
+    }
   }
-
 }
 </style>
