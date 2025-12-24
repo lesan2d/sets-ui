0d5c60145089941229027f857cda1810555d40a4
lesan <lesan2d@qq.com>
2025-12-24 19:19:12 +0800

    feat: :sparkles: 完善 namespace

--- packages/hooks/use-namespace.ts
+++ packages/hooks/use-namespace.ts
@@ -1,20 +1,44 @@
 import { useGlobalConfig } from '@sets-ui/hooks';
 
-export const useNamespace = (block: string) => {
-  const { namespace, themes } = useGlobalConfig();
-
-  const t = () => themes.map(theme => `${namespace}_${block}_${theme}`);
-
-  const b = () => `${namespace}_${block}`;
-
-  const e = (element: string) => `${namespace}_${block}-${element}`;
-
-  const is = (name: string, state: boolean) => name && state ? `is-${name}` : '';
-
-  return {
-    t,
-    b,
-    e,
-    is,
-  }
+interface BEMOptions {
+	namespace: string;
+	block: string;
+	element?: string;
+	modifier?: string;
 }
+
+const bem = ({ namespace, block, element, modifier }: BEMOptions) => {
+	let cls = `${namespace}-${block}`;
+
+	if (element) cls += `__${element}`;
+	if (modifier) cls += `--${modifier}`;
+
+	return cls;
+};
+
+export const useNamespace = (block: string) => {
+	const { namespace, themes = [] } = useGlobalConfig();
+
+	const bemClass = (options?: Pick<BEMOptions, 'element' | 'modifier'>) => bem({ namespace, block, ...options });
+
+	const b = () => bemClass();
+
+	const e = (element: string) => bemClass({ element });
+
+	const m = (modifier?: string) => bemClass({ modifier });
+
+	const em = (element: string, modifier?: string) => bemClass({ element, modifier });
+
+	const t = () => themes.map((theme) => bemClass({ modifier: `theme-${theme}` }));
+
+	const is = (name: string, state: boolean) => (name && state ? `is-${name}` : '');
+
+	return {
+		b,
+		e,
+		m,
+		em,
+		t,
+		is,
+	};
+};
