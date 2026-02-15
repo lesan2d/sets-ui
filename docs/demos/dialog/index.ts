22363be1923c3dde92db07d095b9dd5831a8b609
lesan2d <lesan2d@qq.com>
2026-02-15 18:26:01 +0800

    fix: :green_heart: 修复 Action 错误

--- docs/demos/dialog/index.ts
+++ docs/demos/dialog/index.ts
@@ -1,7 +1,7 @@
 import { withInstall } from '@nopon-web/utils/vue';
-import BasicInstace from './dialog-Basic.vue';
-import SlotInstace from './dialog-Slot.vue';
-import DirectionInstace from './dialog-Direction.vue';
+import BasicInstace from './dialog-basic.vue';
+import SlotInstace from './dialog-slot.vue';
+import DirectionInstace from './dialog-direction.vue';
 
 export const Basic = withInstall({
   ...BasicInstace,
